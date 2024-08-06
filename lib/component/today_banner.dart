4b518b58382d1efca3d2b1e9cfeea000558daeaf
djcommunity <djcommunity20@gmail.com>
2024-08-06 17:25:31 +0900

    first commit

---
+++ lib/component/today_banner.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:calendar_scheduler/const/colors.dart';
+
+class TodayBanner extends StatelessWidget {
+  final DateTime selectedDate;
+  final int count;
+
+  const TodayBanner({super.key, required this.selectedDate, required this.count});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    );
+
+    return Container(
+      color: PRIMARY_COLOR,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일', style: textStyle,),
+            Text('$count개', style: textStyle,),
+          ]
+        ),
+      )
+    );
+  }
+}
